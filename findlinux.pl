1c2646ec7002e74747164decc628bd0acc6b8aac
LeiYangGH <675686066@qq.com>
2017-07-02 18:14:46 +0800

    findlinux.pl

---
+++ findlinux.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use warnings;
+use File::Path;
+use File::Glob ':glob';
+use File::Basename;
+use File::Spec::Functions 'catfile';
+ use File::Find qw(finddepth);
+
+my $testdir = 'C:/test';
+ # my $cdir = 'C:/G/CConsole/CStu';
+my $cdir = '/';
+my @files;
+
+finddepth(sub 
+{
+      return if($_ eq '.' || $_ eq '..');
+      push @files, $File::Find::name;
+}, $cdir);
+for my $f (@files)
+{
+        if(!-d $f &&  $f =~ /python/)
+        { 
+		
+		   
+		      say "-------------$f";
+	}
+	}
+	
+     
+
+print "Hello World!\n";
